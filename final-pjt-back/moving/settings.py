1b7d413b6f0feef86519474a9d8432654754dd63
youngchan choi <youngchan419@gmail.com>
2022-07-05 11:38:59 +0900

    deploy backend

--- final-pjt-back/moving/settings.py
+++ final-pjt-back/moving/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q+pea4aef=wl-3xnecn(#=@5ksa=1!n-1uby01u%xav31&$@76'
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mjyc-back.herokuapp.com']
 
 
 # Application definition
@@ -77,6 +79,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -160,6 +163,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -178,3 +182,8 @@ REST_AUTH_SERIALIZERS = {
 REST_AUTH_REGISTER_SERIALIZERS = {
     'REGISTER_SERIALIZER': 'accounts.serializers.CustomRegisterSerializer',
 }
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
--- final-pjt-front/server.js
+++ final-pjt-front/server.js
@@ -1,13 +1,38 @@
-const express = require('express')
-const serverStatic = require('serve-static')
-const path = require('path')
+// const express = require('express')
+// const serverStatic = require('serve-static')
+// const path = require('path')
 
-const app = express()
-const port = process.env.PORT || 3000
+// const app = express()
+// const port = process.env.PORT || 3000
 
-// SERVES FILES from our dist directory which now contains out index.html
-app.use('/', serverStatic(path.join(__dirname, '/dist')))
-app.listen(port)
-console.log('Listening on port:' + port)
-// app.get('/', (req, res) => res.send('Hello World!'))
-// app.listen(port, () => console.log(`Example app listening on port port!`))
+// // SERVES FILES from our dist directory which now contains out index.html
+// app.use('/', serverStatic(path.join(__dirname, '/dist')))
+// app.listen(port)
+// console.log('Listening on port:' + port)
+// // app.get('/', (req, res) => res.send('Hello World!'))
+// // app.listen(port, () => console.log(`Example app listening on port port!`))
+
+
+const express = require('express');
+const path = require('path');
+const history = require('connect-history-api-fallback');
+
+const app = express();
+
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+
+app.use(staticFileMiddleware);
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}));
+app.use(staticFileMiddleware);
+
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/index.html'));
+});
+
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
